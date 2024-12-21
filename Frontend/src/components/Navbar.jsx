d51fded971960cf57140eb6a0bdde6c4bcd1e631
Swagat003 <swagat.pritam.2003@gmail.com>
2024-12-21 18:48:08 +0530

    Add mongoose model initialization and remove unused .env file

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
-import { NavLink, Link } from 'react-router-dom'
+import { useState } from 'react'
+import { NavLink, Link, useNavigate } from 'react-router-dom'
 import './css/Navbar.css'
 
 function Navbar() {
+  const navigate = useNavigate()
+  const [isLogged, setIsLogged] = useState(true)
+  const [Name, setName] = useState("Swagat")
+  function logout() {
+    setIsLogged(false)
+    navigate('/')
+  }
   return (
     <div id='navbar'>
-        <Link to="/" id='icon-name'>
-            <img src="./document.png" alt="logo" />
-            <h1>Collab Docs</h1>
-        </Link>
-        <div id='menu'>
-            <NavLink className={(e) => {return e.isActive? "active-link": ""}} to='/dashboard'>Dashboard</NavLink>
-            <NavLink className={(e) => {return e.isActive? "active-link": ""}} to='/login'>Login</NavLink>
-            <NavLink className={(e) => {return e.isActive? "active-link": ""}} to='/signup'>Signup</NavLink>
-        </div>
+      <Link to="/" id='icon-name'>
+        <img src="./document.png" alt="logo" />
+        <h1>Collab Docs</h1>
+      </Link>
+      <div id='menu'>
+        {
+          isLogged ? (
+            <>
+              <NavLink className={(e) => e.isActive ? "active-link" : ""} to='/dashboard'>Dashboard</NavLink>
+              <div id='profile-name'>Hi! {Name}👋</div>
+              <button id='logedout' onClick={() => logout()}>Logout</button>
+            </>
+          ) : (
+            <>
+              <NavLink className={(e) => e.isActive ? "active-link" : ""} to='/login'>Login</NavLink>
+              <NavLink className={(e) => e.isActive ? "active-link" : ""} to='/signup'>Signup</NavLink>
+            </>
+          )
+        }
+      </div>
     </div>
   )
 }
